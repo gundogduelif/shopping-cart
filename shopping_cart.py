03a07c7a1f92cc5817da41bba90d4960aaf9e78c
gundogduelif <eg3068@stern.nyu.edu>
2020-06-10 13:40:32 -0400

    Checkpoint 1: Capturing User Inputs

--- shopping_cart.py
+++ shopping_cart.py
@@ -35,6 +35,70 @@ def to_usd(my_price):
     """
     return f"${my_price:,.2f}" #> $12,000.71
 
-# TODO: write some Python code here to produce the desired output
+product_ids = []         #The Infinite Loop REFERENCE : https://www.tutorialspoint.com/python/python_while_loop.htm
+var = 1               
+while var == 1 :  
+   product_id = input('Please input a product identifier:')
+   if product_id == "DONE":
+       break
+   else: 
+       product_ids.append(product_id)     # Python List append() REFERENCE : https://www.youtube.com/watch?v=3BaGb-1cIr0&feature=youtu.be
+print("SHOPPING CARD IDENTIFIERS:",(product_ids))
 
-print(products)
+# product_ids = products["id"]
+
+
+
+# Checkpoint 2: Look-up Products  
+  # Steps:
+
+  # 1.For a single valid product identifier, look up the matching product and print its name and price. HINT: try using a custom function in conjunction with a list comprehension.
+  # 2.For each valid product identifier in the example list, look up the matching product and print its name and price.
+  # 3.For each valid product identifier in the example list, look up the matching product and print its name and price, and add its price to a running-total of all prices, then print the running-total after iterating through the entire list. For now, you don't necessarily need to worry about formatting prices as USD.
+
+# Checkpoint 3: Printing the Receipt
+  # Steps:
+
+  # 1. For each receipt component listed in the project requirements (e.g. store name, product prices, taxes, total price, farewell message, etc.), revise your program to print that component.
+
+# TO DO LIST
+
+#####  1 A grocery store name of your choice
+##### 2 A grocery store phone number and/or website URL and/or address of choice
+# 3 The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2020-02-07 03:54 PM)
+# 4 The name and price of each shopping cart item, price being formatted as US dollars and cents (e.g. $3.50, etc.)
+# 5 The total cost of all shopping cart items (i.e. the "subtotal"), formatted as US dollars and cents (e.g. $19.47), calculated as the sum of their prices
+# 6 The amount of tax owed (e.g. $1.70), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax)
+# 7 The total amount owed, formatted as US dollars and cents (e.g. $21.17), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items
+# ##### 8 A friendly message thanking the customer and/or encouraging the customer to shop again
+
+
+
+# Please input a product identifier: 1
+# Please input a product identifier: 2
+# Please input a product identifier: 3
+# Please input a product identifier: 2
+# Please input a product identifier: 1
+# Please input a product identifier: DONE
+print("---------------------------------")
+print("GOURMET FOODS GROCERY")
+print("WWW.GOURMETFOODSGROCERY.COM")
+print("Address: 1508 Kings Hwy, Brooklyn, NY 11229")
+print("P:(917) 222-3344")
+print("---------------------------------")
+print("CHECK OUT AT:")
+#> CHECKOUT AT: 2020-02-07 03:54 PM
+print("---------------------------------")
+print("SELECTED PRODUCTS:")
+#>  ... Chocolate Sandwich Cookies ($3.50)
+#>  ... All-Seasons Salt ($4.99)
+#>  ... Robust Golden Unsweetened Oolong Tea ($2.49)
+#>  ... All-Seasons Salt ($4.99)
+#>  ... Chocolate Sandwich Cookies ($3.50)
+print("---------------------------------")
+print("SUBTOTAL:")
+print("TAX:")
+print("TOTAL:")
+print("---------------------------------")
+print("THANKS, SEE YOU AGAIN SOON!")
+print("---------------------------------")
